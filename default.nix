e29259e564ffa555d379ac20fbf42014fabd2b31
AveryanAlex <alex@averyan.ru>
2023-01-15 14:45:29 +0300

    Basic server

--- default.nix
+++ default.nix
@@ -9,15 +9,39 @@ let
       luckperms
       coreprotect
     ];
+    configs = [
+      (mineflake.mkMfConfig "raw" "server.properties" ''
+        enable-command-block=false
+        server-ip=0.0.0.0
+        server-port=25000
+        query.port=25000
+        online-mode=false
+      '')
+    ];
   };
 
   proxy = pkgs.mineflake.buildMineflakeBin {
     type = "bungee";
     command = "${pkgs.jre_headless}/bin/java -Xms1G -Xmx1G -jar {}";
     package = pkgs.mineflake.waterfall;
-    # plugins = with pkgs.mineflake; [
-      # luckperms
-    # ];
+    configs = [
+      (mineflake.mkMfConfig "mergeyaml" "config.yml" {
+        online_mode = false;
+        listeners = [
+          {
+            host = "0.0.0.0:25565";
+            query_port = 25565;
+            motd = "&1Firesquare V2!";
+            max_players = 20;
+          }
+        ];
+        servers = {
+          lobby = {
+            address = "127.0.0.1:25000";
+          };
+        };
+      })
+    ];
   };
 in
 {
@@ -30,7 +54,7 @@ in
       ExecStart = "${lobby}/bin/mineflake";
       User = "fire-lobby";
       Group = "fire-minecraft";
-      WorkingDirectory = "/var/lib/fire-lobby";
+      WorkingDirectory = "/var/lib/firesquare/lobby";
     };
   };
 
@@ -38,7 +62,7 @@ in
     isSystemUser = true;
     createHome = true;
     group = "fire-minecraft";
-    home = "/var/lib/fire-lobby";
+    home = "/var/lib/firesquare/lobby";
   };
 
   systemd.services.fire-proxy = {
@@ -50,7 +74,7 @@ in
       ExecStart = "${proxy}/bin/mineflake";
       User = "fire-proxy";
       Group = "fire-minecraft";
-      WorkingDirectory = "/var/lib/fire-proxy";
+      WorkingDirectory = "/var/lib/firesquare/proxy";
     };
   };
 
@@ -58,7 +82,7 @@ in
     isSystemUser = true;
     createHome = true;
     group = "fire-minecraft";
-    home = "/var/lib/fire-proxy";
+    home = "/var/lib/firesquare/proxy";
   };
 
   users.groups.fire-minecraft = { };
